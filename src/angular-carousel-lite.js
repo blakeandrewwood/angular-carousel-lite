872154bbe62110a1f80da390b520fcda8f3accc5
Blake <blakeandrewwood@gmail.com>
2015-09-08 08:25:49 +0900

    update info

--- src/angular-carousel-lite.js
+++ src/angular-carousel-lite.js
@@ -1,14 +1,4 @@
 /* jshint node: true */
 
-/*
-* CarouselLite
-* Version: 0.0.2
-* Author: Blake Wood	
-* Website: blakewood.co
-* Docs: http://github.com/blakeandrewwood/angular-carousel-lite 
-* Repo: http://github.com/blakeandrewwood/angular-carousel-lite 
-* Issues: http://github.com/blakeandrewwood/angular-carousel-lite/issues 
-* Dependencies: Angular, jQuery
-*/
 'use strict';
 angular.module('angularCarouselLite', []);
--- src/directives/carousel-lite.js
+++ src/directives/carousel-lite.js
@@ -1,15 +1,5 @@
 /* jshint node: true */
 
-/*
-* CarouselLite
-* Version: 0.0.2
-* Author: Blake Wood	
-* Website: blakewood.co
-* Docs: http://github.com/blakeandrewwood/angular-carousel-lite 
-* Repo: http://github.com/blakeandrewwood/angular-carousel-lite 
-* Issues: http://github.com/blakeandrewwood/angular-carousel-lite/issues 
-* Dependencies: Angular, jQuery
-*/
 'use strict';
 angular.module('angularCarouselLite')
 .directive('carouselLite', ['$timeout', function ($timeout) {
